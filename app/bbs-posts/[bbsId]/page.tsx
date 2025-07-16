e74ed5781e3b3b0742eb4801582ba05acc0baee1
taisuke91 <itoutaisuke20030430@gmail.com>
2025-07-16 21:00:51 +0900

    これで動くはず！！！

--- app/bbs-posts/[bbsId]/page.tsx
+++ app/bbs-posts/[bbsId]/page.tsx
@@ -1,30 +1,14 @@
-"use client";
 import { BBSData } from '@/app/types/type';
+import prisma from "../../../lib/prismaClient";
 import Link from 'next/link';
 import React from 'react'
-const API_BASE_URL: string = process.env.NEXT_PUBLIC_API_BASE_URL || '';
 
-async function getDetailBBSData(id: number) {
-  const apiBaseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}` // Vercelにデプロイされた場合
-    : 'http://localhost:3000';           // ローカル開発環境の場合
-  
-  const response = await fetch(`/api/post/${id}`, {
-    cache: "no-store",
+async function getDetailBBSData(id: string) {
+  const bbsDetailData = await prisma.post.findUnique({
+    where: {
+      id: parseInt(id),
+    },
   });
-  // fetch関数を使って指定されたURLに対してHTTPリクエストを送信しています。
-  // awaitキーワードは、fetchリクエストが完了してレスポンスが返ってくるまで、この関数の実行を一時停止します。
-  // レスポンスが返ってくると、そのResponseオブジェクトがresponse定数に代入されます。
-  // cache: "no-store"は、このリクエストの結果をブラウザやその他のキャッシュに保存しないように指示しています。
-  // これにより、常にサーバーから最新のデータが取得されることが保証されます。
-
-  const bbsDetailData: BBSData = await response.json();
-  // 前の行で取得したresponseオブジェクトは、生のHTTPレスポンスを含んでいます。
-  // response.json()メソッドは、そのレスポンスボディをJSON形式として
-  // パース（解析）し、JavaScriptのオブジェクトまたは配列に変換する非同期操作です。
-  // JSON(JavaScript Object Notation)とは、データを表現するための
-  // 軽量なテキストベースのフォーマットです。
-
   return bbsDetailData;
 }
 
@@ -34,23 +18,33 @@ async function getDetailBBSData(id: number) {
 //さらにそのparamsオブジェクトはbbsIdというプロパティを持ち、
 // そのbbsIdプロパティの値はnumber型である」という意味になります。
 
-const BBSDetailPage = async ({ params }: { params: Promise<{ bbsId: number }> }) => {
+const BBSDetailPage = async ({ params }: { params: Promise<{ bbsId: string }> }) => {
+  
   const bbsDetailData = await getDetailBBSData((await params).bbsId);
-  const { title, content, username } = bbsDetailData;
-  return (
-    <div className="mx-auto max-w-4xl p-4">
-      <div className="mb-8">
-        <h1 className="text-2xl font-bold">{title}</h1>
-        <p className="text-gray-700">{username}</p>
-      </div>
+  if (bbsDetailData) {
+    const { title, content, username } = bbsDetailData;
+    return (
+      <div className="mx-auto max-w-4xl p-4">
+        <div className="mb-8">
+          <h1 className="text-2xl font-bold">{title}</h1>
+          <p className="text-gray-700">{username}</p>
+        </div>
 
-      <div className="mb-8">
-        <p className="text-gray-900">{content}</p>
-      </div>
+        <div className="mb-8">
+          <p className="text-gray-900">{content}</p>
+        </div>
 
-      <Link href={"/"} className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md">戻る</Link>
-    </div>
-  );
+        <Link href={"/"} className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md">戻る</Link>
+      </div>
+    );
+  } else {
+    return (
+      <div className="mx-auto max-w-4xl p-4">
+        <h1 className="text-2xl font-bold">投稿が見つかりません</h1>
+        <Link href={"/"} className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md">戻る</Link>
+      </div>
+    );
+  }
 };
 
 export default BBSDetailPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,9 @@
 import BBSCardList from "./components/BBSCardList"; //BBSCardListコンポーネントのインポート
 import { BBSData } from "./types/type";  //BBSData型のインポート(型定義ファイルから)
-const API_BASE_URL: string = process.env.NEXT_PUBLIC_API_BASE_URL || ''; //環境変数からAPIのベースURLを取得
 import prisma from "../lib/prismaClient";
 
 async function getBBSAllData() {
-  const apiBaseUrl = process.env.VERCEL_URL
-    ? `https://${process.env.VERCEL_URL}` // Vercelにデプロイされた場合
-    : 'http://localhost:3000';           // ローカル開発環境の場合
-  
-  console.log("apiBaseUrl:", apiBaseUrl);
   const bbsAllData = await prisma.post.findMany();
-  //const response = await fetch(`${apiBaseUrl}/api/post`, {
-  //  cache: "no-store",
-  //});
-  {/*
-    ここでは、fetch関数を使って指定されたURL (http://localhost:3000/api/post) に対してHTTPリクエストを送信しています。
-    awaitキーワードは、fetchリクエストが完了してレスポンスが返ってくるまで、この関数の実行を一時停止します。
-    レスポンスが返ってくると、そのResponseオブジェクトがresponse定数に代入されます。
-    cache: "no-store"は、このリクエストの結果をブラウザやその他のキャッシュに保存しないように指示しています。これにより、常にサーバーから最新のデータが取得されることが保証されます。
-  */}
-  //console.log("response:", response);
-  //エラーが出ているのでここで終わっている。
-  // (後ろのconsole.logは実行されない)
-  
-  //const bbsAllData: BBSData[] = await response.json();
-  {/*
-    前の行で取得したresponseオブジェクトは、生のHTTPレスポンスを含んでいます。
-    response.json()メソッドは、そのレスポンスボディをJSON形式としてパース（解析）し、
-    JavaScriptのオブジェクトまたは配列に変換する非同期操作です。
-    JSON(JavaScript Object Notation)とは、データを表現するための軽量なテキストベースのフォーマット
-    ここでもawaitキーワードが使用されており、JSONのパースが完了するまで処理を一時停止し、
-    変換されたデータがbbsAllData定数に代入されます。
-    BBSData[]は定義した型の名前で、これはBBSData型のオブジェクトの配列であることを示しています。
-  */}
-
-  console.log("bbsAllData:", bbsAllData);
-  
   return bbsAllData;
 }
 
