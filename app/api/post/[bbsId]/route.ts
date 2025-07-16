c167052f4081af377dfa128cb57939b980ccfd3b
taisuke91 <itoutaisuke20030430@gmail.com>
2025-07-16 18:11:17 +0900

    ちょっと変更

--- app/api/post/[bbsId]/route.ts
+++ app/api/post/[bbsId]/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import prisma from "../../../../lib/prismaClient";
 
 export async function GET(
+    _req: Request,
     { params }: { params: Promise<{ bbsId: string }> }
     //bbsIdというプロパティを持つparamsオブジェクトをparamsという名前で受け取ります。
 ) {
